f9a26e934ac573a09bed937eb0a3728825cd9fee
Linne13 <a_kiddo13@hotmail.com>
2017-02-12 22:18:20 +0600

    LAB1

---
+++ HW/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW
+{
+    class Student
+    {
+        public int age;
+        public string name;
+        public string address;
+
+   
+        public Student(string name, string address, int age)
+        {
+            this.age = age;
+            this.address = address;
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return age + " " + "years old;" + " " + "name:" + name + ";" + " " + "hometown: " + address;
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Student s = new Student("Kurosaki", "Karakura", 17);
+            Console.WriteLine(s.ToString());
+            Console.ReadKey();
+        }
+    }
+}
