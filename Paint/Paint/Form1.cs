c328d475de9fe2adc5ec4151b0d4bdb8c4541d9c
Alex <a_kiddo13@hotmail.com>
2017-04-10 11:50:48 +0600

    Add files via upload

---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MyPaint mp = new MyPaint();
+
+        Graphics g;
+        Bitmap bmp;
+
+        public void colorBtnClicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            mp.setPenColor(btn.BackColor);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mp.mouseClicked = false;
+  
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mp.mouseClicked = true;
+            mp.prevPoint = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mp.mouseClicked)
+            {
+                mp.Draw(pictureBox1.CreateGraphics(), e.Location);
+            }
+        }
+
+    }
+
+}
---
+++ Paint/Paint/MyPaint.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Paint
+{
+    public class MyPaint
+    {
+
+         public enum Shape
+         {
+            LINE,
+            RECTANGLE,
+            ELLIPSE,
+            FREE,
+            FILL
+        };
+
+        Pen pen = new Pen(Color.Black);
+        Brush brush = new SolidBrush(Color.Black);
+        public Point prevPoint = new Point(0, 0);
+        public bool mouseClicked = false;
+        public Shape sh = Shape.LINE;
+
+
+        public void setPenColor(Color color)
+        {
+            pen.Color = color;
+        }
+
+        public void Draw(Graphics g, Point curPoint)
+        {
+            if (mouseClicked)
+            {
+                if (sh == Shape.LINE)
+                {
+                    g.DrawLine(pen, prevPoint, curPoint);
+                    prevPoint = curPoint;
+                }
+                if (sh == Shape.RECTANGLE)
+                {
+                   g.DrawRectangle(pen, prevPoint, curPoint, )
+                }
+            }
+        }
+
+
+    }
+}
